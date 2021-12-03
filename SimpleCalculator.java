56d2932ad525fe6b45e602dcb2156bfd5550747c
Defeaded-S <72038136+Defeaded-S@users.noreply.github.com>
2021-12-03 19:31:47 +0300

    Create SimpleCalculator.java

---
+++ SimpleCalculator.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int second = Integer.valueOf(scanner.nextLine());
+        int sum = (first + second);
+        int subt = (first - second);
+        int mult = (first * second);
+        double div = ((double) first / second);
+        System.out.println(first + " + " + second + " = " + sum);
+        System.out.println(first + " - " + second + " = " + subt);
+        System.out.println(first + " * " + second + " = " + mult);
+        System.out.println(first + " / " + second + " = " + div);
+
+    }
+}
